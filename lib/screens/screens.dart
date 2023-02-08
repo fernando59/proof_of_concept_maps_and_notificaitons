1b8055001db0c622ecbc9054a08e83493ec97c23
Fernando <fernandomercado529@gmail.com>
2023-02-08 00:15:19 -0400

    Add structure proyect

---
+++ lib/screens/screens.dart
@@ -0,0 +1,3 @@
+export 'package:proof_of_concept_maps_and_notifications/screens/loading_screen.dart';
+export 'package:proof_of_concept_maps_and_notifications/screens/gps_access_screen.dart';
+export 'package:proof_of_concept_maps_and_notifications/screens/map_screen.dart';
