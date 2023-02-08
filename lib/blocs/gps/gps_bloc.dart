471be25c85c27e5cb904c4a00b86bb4b7afa5bf2
Fernando <fernandomercado529@gmail.com>
2023-02-08 01:06:39 -0400

    Add geolocator and check if is enable the service

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -11,5 +12,25 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
     on<GpsAndPermissionEvent>((event, emit) => emit(state.copyWith(
         isGpsEnable: event.isGpsEnable,
         isGpsPermissionGranted: event.isGpsPermissionGranted)));
+    _init();
+  }
+
+  Future<void> _init() async {
+    final isEnabled = await _checkGpsStatus();
+    print('is enabled $isEnabled');
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = await Geolocator.isLocationServiceEnabled();
+    Geolocator.getServiceStatusStream().listen((event) {
+      print('service status $event');
+    });
+    return isEnable;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: implement close
+    return super.close();
   }
 }
